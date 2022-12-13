158870ad3247e4997ddcc98eee2fcafb98f0c3cc
Terence Martin <github@nurdz.com>
2022-12-12 21:43:08 -0800

    Include a simple documentation link in the footer
    Since we have a documentation site now, add a ham handed link to it in
    the status bar. This currently uses a book icon because there is no
    greybeard face and the wizard hat seems mildly esoteric since the app
    is not that magical yet.

--- src/client/components/Icon.svelte
+++ src/client/components/Icon.svelte
@@ -1,8 +1,8 @@
 <script>
-    export let name;
-    export let size;
+  export let name;
+  export let size;
 
-    $: [icon, base = "solid"] = (name || "").split(":");
+  $: [icon, base = "solid"] = (name || "").split(":");
 </script>
 
 <font-awesome-icon style="font-size: {size};" class="fa-{base} fa-{icon}" />
