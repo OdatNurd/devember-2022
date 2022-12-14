79767d871605e6b5e11d2e964e443d689b6fd88d
Terence Martin <github@nurdz.com>
2022-12-13 23:46:56 -0800

    Populate the dashboard using our panel API
    Although the API is still using canned data, this fleshes out the code
    path for populating the panels dynamically into the dashboard at load
    time.
    
    More work needs to be done here, such as a better loading text, and
    the fact that we probably need a tab control of some sort and several
    grids, since workspaces.

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { NavBar, Dashboard, DashboardPanel } from '$components';
+  import { NavBar, Dashboard } from '$components';
 </script>
 
 <div class="flex flex-col h-screen">
@@ -8,10 +8,7 @@
   <div class="flex flex-1 w-full overflow-hidden p-0 m-0">
     <div class="bg-base-100 h-full w-full m-0 p-0">
       <div class="w-full text-base-content m-0">
-        <Dashboard>
-          <DashboardPanel title={'Panel 1'} width={2} height={2} content={'Sample Content 1'} />
-          <DashboardPanel title={'Panel 2'} width={2} height={3} />
-        </Dashboard>
+        <Dashboard />
       </div>
     </div>
   </div>
--- src/client/components/dashboard/Dashboard.svelte
+++ src/client/components/dashboard/Dashboard.svelte
@@ -1,8 +1,21 @@
 <script>
-  import { onMount } from 'svelte';
+  import { DashboardPanel } from '$components';
 
-  onMount(() => {
-    let grid = GridStack.init({
+  // The grid stack instance; this gets created only after all of the panels
+  // load, in the reactive block below.
+  let grid = null;
+  let panelWrapper = null;
+
+  const fetchUIPanels = async () => {
+    const res = await fetch('/api/v1/panels');
+    return res.json();
+  }
+
+  // When the main component body below renders out all of the fetched panel
+  // components, it binds the panelWrapper to the wrapper div, which allows
+  // this code to fire and initialize the grid.
+  $: if (grid === null && panelWrapper !== null) {
+    grid = GridStack.init({
       cellHeight: 'initial',
       float: true,
       margin: 4,
@@ -13,12 +26,17 @@
       // layout will change to a single column.
       disableOneColumnMode: false,
     });
-  });
-
+  }
 </script>
 
 <div class="grid-holder">
-  <div class="grid-stack">
-    <slot></slot>
-  </div>
+  {#await fetchUIPanels()}
+    panels be loading
+  {:then panels}
+    <div class="grid-stack" bind:this={panelWrapper}>
+      {#each panels as panel (panel.name)}
+        <DashboardPanel {...panel} />
+      {/each}
+    </div>
+  {/await}
 </div>
